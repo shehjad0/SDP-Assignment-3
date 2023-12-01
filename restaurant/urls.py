5e5761ba1aaf2bef24865df8d2a30ecc8ec64ea9
shehjad0 <bettercallshehjad@gmail.com>
2023-12-01 23:35:41 +0600

    Done

---
+++ restaurant/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.index, name="home"),
+    path("meal/", include("meal.urls")),
+    path("about_us/", include("about_us.urls")),
+]
